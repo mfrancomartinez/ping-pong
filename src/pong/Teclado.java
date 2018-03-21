6023c09b8c2f95b4bd70285dc3813c6a8c319bdc
Marcos <Marcos@192.168.1.5>
2018-03-21 07:09:43 +0100

    pelota2

--- src/pong/Teclado.java
+++ src/pong/Teclado.java
@@ -13,34 +13,34 @@ public class Teclado extends KeyAdapter {//permite trabajar con eventos de tecla
         int id= e.getKeyCode();
         if(id== KeyEvent.VK_W){
             w=true;
-            System.out.println("w");
+            //System.out.println("w");
         }
          if(id== KeyEvent.VK_S){
             s=true;
-            System.out.println("s=true");
+            //System.out.println("s=true");
         }
           if(id== KeyEvent.VK_UP){
             up=true;
-            System.out.println("up");
+            //System.out.println("up");
         }
            if(id== KeyEvent.VK_DOWN){
             down=true;
-            System.out.println(down);
+            //System.out.println(down);
         }
     }
      public void KeyReleased(KeyEvent e){//aparece cada vex que soltamos una tecla
           int id= e.getKeyCode();
         if(id == KeyEvent.VK_W){
             w=false;
-            System.out.println(w);
+            //System.out.println(w);
         }
          if(id == KeyEvent.VK_S){
             s=false;
-            System.out.println(s);
+            //System.out.println(s);
         }
           if(id == KeyEvent.VK_UP){
             up=false;
-            System.out.println(up);
+            //System.out.println(up);
         }
            if(id == KeyEvent.VK_DOWN){
             down=false;
