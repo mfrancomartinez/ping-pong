0396e1e1b7aa0d60a56c89676264172afa0665b3
Marcos <Marcos@192.168.1.5>
2018-03-21 06:36:49 +0100

    hilo

---
+++ src/pong/Pelota.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pong;
+
+    
+import java.awt.Rectangle;
+import java.awt.geom.Rectangle2D;
+
+public class Pelota {
+    private int x,y;
+    private int a=1,b=1;
+    private final int ancho=15,alto=15;
+    public Pelota(int x,int y){
+        this.x=x;
+        this.y=y;
+    
+}
+    public Rectangle2D getPelota(){
+        return new Rectangle2D.Double(x,y,ancho,alto) ;
+    }
+    public void mover(Rectangle limites){
+        x+=a;
+        y+=b;
+        if(x>limites.getMaxX()){
+         a=-a;   
+        }
+         if(y>limites.getMaxY()){
+         b=-b;   
+        }
+         if(x<0){
+             a=-a;
+         }
+         if(y<0){
+        b=-b;
+    }
+    }
+    
+}
